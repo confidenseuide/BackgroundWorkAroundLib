fb86198a276816e002c73c2819d276b890fff83b
confidenseuide <confidenseuide@proton.me>
2026-02-22 01:08:02 +0500

    Update WatcherService.java

--- app/src/main/java/protectedwp/safespace/WatcherService.java
+++ app/src/main/java/protectedwp/safespace/WatcherService.java
@@ -1,10 +1,17 @@
 package protectedwp.safespace;
 
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 
 public class WatcherService extends Service {
