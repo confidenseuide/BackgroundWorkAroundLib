5291aa90ad2e7ce8badf3eccc577882f4f64001d
confidenseuide <confidenseuide@proton.me>
2026-02-22 02:57:52 +0500

    Create MainActivity.java

---
+++ app/src/main/background/work/around/MainActivity.java
@@ -0,0 +1,28 @@
+package background.work.around;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        Button runBtn = new Button(this);
+        runBtn.setText("START");
+        setContentView(runBtn);
+
+        runBtn.setOnClickListener(v -> {
+            
+            Intent intent = new Intent("background.work.around.START_NUCLEUS");
+            
+            intent.setPackage("background.work.around");
+            
+            sendBroadcast(intent);
+        });
+    }
+}
