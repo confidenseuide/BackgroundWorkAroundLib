1bc46f78afa289ca25ad634bb0af39543f9e2aac
confidenseuide <confidenseuide@proton.me>
2026-02-22 16:23:25 +0500

    Update WatcherService.java

--- app/src/main/java/background/work/around/WatcherService.java
+++ app/src/main/java/background/work/around/WatcherService.java
@@ -17,7 +17,8 @@ public class WatcherService extends Service {
     private void bindToNeighbor() {
     Intent intent = new Intent(this, WatcherService2.class);
     bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);
-    startService(intent); 
+    try {startService(intent);} 
+    catch (Throwable t) {}
     }
     
     private final ServiceConnection connection = new ServiceConnection() {
