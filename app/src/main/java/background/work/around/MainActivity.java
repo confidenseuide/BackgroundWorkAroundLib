29c9be0de55f13620137e6754edacce3e61c5915
confidenseuide <confidenseuide@proton.me>
2026-02-23 20:23:39 +0500

    Update MainActivity.java

--- app/src/main/java/background/work/around/MainActivity.java
+++ app/src/main/java/background/work/around/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onPause() {
     super.onPause();    
+    // System may kills apps after user-forced clearing of the task stack. No task — no problem.
     finishAndRemoveTask();
     }
 
