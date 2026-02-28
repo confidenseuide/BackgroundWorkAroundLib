f03305c08e720bb39998ff77480c356a75c989be
confidenseuide <confidenseuide@proton.me>
2026-02-28 07:00:00 +0500

    Update HelperService.java

--- app/src/main/java/background/work/around/HelperService.java
+++ app/src/main/java/background/work/around/HelperService.java
@@ -33,7 +33,7 @@ public class HelperService extends Service {
 
 	private void forceBindAndStart() {
     Intent intent = new Intent(this, RiderService.class);
-    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT);
+    bindService(intent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);
     try {startService(intent);} 
     catch (Throwable t) {}
     }
