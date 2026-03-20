e3a9e0237efb14346e2a27702f7a99b17c874859
confidenseuide <252536920+confidenseuide@users.noreply.github.com>
2026-03-21 04:38:53 +0500

    Update RiderService.java

--- example/RiderService.java
+++ example/RiderService.java
@@ -2,9 +2,8 @@
 
 /*
 To start use:
-Intent intent = new Intent("background.work.around" + ".START");
-intent.setPackage(getPackageName());            
-sendBroadcast(intent);
+background.work.around.Start.RunService(this);
+or your context instead of "this".
 */
 
 /*
@@ -12,7 +11,7 @@ ATTENTION: THIS IS A DEMONSTRATIONAL PROJECT. IF YOU DO NOT OVERRIDE ALL METHODS
 ВНИМАНИЕ: ЭТО ДЕМОНСТРАЦИОННЫЙ ПРОЕКТ. ЕСЛИ ВЫ НЕ ПЕРЕОПРЕДЕЛИТЕ ВСЕ МЕТОДЫ, ОН МОЖЕТ ВОСПРОИЗВОДИТЬ ЗВУК ДЛЯ ДЕМОНСТРАЦИИ СВОЕЙ РАБОТЫ. В ЭТОМ ПРИМЕРЕ УЖЕ ВСЕ МЕТОДЫ ПЕРЕОПРЕДЕЛЕНЫ.
 */
 
-package your.pack.name;
+package your.pack.name; //your package name. (Must match the path).
 
 public class RiderService extends background.work.around.RiderService {
 
